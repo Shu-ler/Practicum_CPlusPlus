3c0390eb06f9acc102e0e007c24686b3e1f9ad46
Filimonov Igor <shuler1970@gmail.com>
2025-07-09 11:25:45 +0700

    Ещё урок

--- sprint 4/topik 2/lesson 4/robot.h
+++ sprint 4/topik 2/lesson 4/robot.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <vector>
 #include <optional>
 
@@ -11,27 +9,53 @@ struct MaxCell {
 class Robot {
 public:
     explicit Robot(size_t max_cells) {
+        line_.resize(max_cells);        // Используя метод вектора resize(), задать размер
+        current_it_ = line_.begin();    // Итератор, который хранит текущую позицию робота
     }
 
     void GoRight(size_t n_pos) {
-        // Ваша реализация метода.
+        if (n_pos < (line_.size() - GetCurrentIndex() - 1)) {
+            current_it_ += n_pos;
+        }
     }
 
     void GoLeft(size_t n_pos) {
-        // Ваша реализация метода.
+        if (n_pos <= GetCurrentIndex()) {
+            current_it_ -= n_pos;
+        }
     }
 
     void SetPos(size_t start) {
-        // Ваша реализация метода.
+        if (start < line_.size()) {
+            current_it_ = line_.begin() + start;
+        }
     }
 
     void PutBalls(int num) {
-        // Ваша реализация метода.
+        (*current_it_) += num;
     }
-
+    
     // Нужно возвратить std::nullopt, если вектор пуст.
     std::optional<MaxCell> CalculateMaxValue() const {
-        // Ваша реализация метода.
+        if (line_.empty()) {
+            return std::nullopt;
+        }
+        
+        MaxCell res{0,0};
+        size_t cur_pos = 0;
+        for (auto i = line_.begin(); i != line_.end(); ++i, ++cur_pos) {
+            if (*i > res.num) {
+                res.num = *i;
+                res.pos = cur_pos;
+            }
+        }
+        
+        return std::optional {res};
+    }
+    
+    // Возвращает текущий порядковый номер позиции
+    size_t GetCurrentIndex() const {
+        return current_it_ - line_.begin();
     }
 
 private:
