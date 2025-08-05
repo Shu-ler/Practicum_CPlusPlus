ef5a89862485e37f1ea0c69037a7c9e8bf2ea1e7
Filimonov Igor <shuler1970@gmail.com>
2025-08-05 15:14:48 +0700

    Урок 4 выполнен

--- sprint6/Lesson4/Lesson4/Lesson4.cpp
+++ sprint6/Lesson4/Lesson4/Lesson4.cpp
@@ -2,69 +2,74 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include "log_duration.h"
 
 using namespace std;
 
 vector<int> ReverseVector(const vector<int>& source_vector) {
-    vector<int> res;
-    for (int i : source_vector) {
-        res.insert(res.begin(), i);
-    }
+	vector<int> res;
+	for (int i : source_vector) {
+		res.insert(res.begin(), i);
+	}
 
-    return res;
+	return res;
 }
 
 int CountPops(const vector<int>& source_vector, int begin, int end) {
-    int res = 0;
+	int res = 0;
 
-    for (int i = begin; i < end; ++i) {
-        if (source_vector[i]) {
-            ++res;
-        }
-    }
+	for (int i = begin; i < end; ++i) {
+		if (source_vector[i]) {
+			++res;
+		}
+	}
 
-    return res;
+	return res;
 }
 
 void AppendRandom(vector<int>& v, int n) {
-    for (int i = 0; i < n; ++i) {
-        // получаем случайное число с помощью функции rand.
-        // с помощью (rand() % 2) получим целое число в диапазоне 0..1.
-        // в C++ имеются более современные генераторы случайных чисел,
-        // но в данном уроке не будем их касаться
-        v.push_back(rand() % 2);
-    }
+	for (int i = 0; i < n; ++i) {
+		// получаем случайное число с помощью функции rand.
+		// с помощью (rand() % 2) получим целое число в диапазоне 0..1.
+		// в C++ имеются более современные генераторы случайных чисел,
+		// но в данном уроке не будем их касаться
+		v.push_back(rand() % 2);
+	}
 }
 
 void Operate() {
-    vector<int> random_bits;
+	using namespace std::literals;
+	vector<int> random_bits;
+	vector<int> reversed_bits;
 
-    // операция << для целых чисел это сдвиг всех бит в двоичной
-    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
-    static const int N = 1 << 17;
+	// операция << для целых чисел это сдвиг всех бит в двоичной
+	// записи числа. Запишем с её помощью число 2 в степени 17 (131072)
+	static const int N = 1 << 17;
 
-    // заполним вектор случайными числами 0 и 1
-    const auto append_start = chrono::steady_clock::now();
-    AppendRandom(random_bits, N);
-    const auto append_end = chrono::steady_clock::now();
-    cerr << "Append random: "s << chrono::duration_cast<chrono::milliseconds>(append_end - append_start).count() << " ms"s << endl;
+	{
+		// заполним вектор случайными числами 0 и 1
+		LogDuration append("Append random"s);
+		AppendRandom(random_bits, N);
+	}
 
-    // перевернём вектор задом наперёд
-    const auto rev_start = chrono::steady_clock::now();
-    vector<int> reversed_bits = ReverseVector(random_bits);
-    const auto rev_end = chrono::steady_clock::now();
-    cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(rev_end - rev_start).count() << " ms"s << endl;
+	{
+		// перевернём вектор задом наперёд
+		LogDuration reverse("Reverse"s);
+		reversed_bits = ReverseVector(random_bits);
+	}
 
-    // посчитаем процент единиц на начальных отрезках вектора
-    const auto count_start = chrono::steady_clock::now();
-    for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
-        double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-        cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
-    }
-    const auto count_end = chrono::steady_clock::now();
-    cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(count_end - count_start).count() << " ms"s << endl;
+	{
+		// посчитаем процент единиц на начальных отрезках вектора
+		LogDuration counting("Counting"s);
+		for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
+			double rate = CountPops(reversed_bits, 0, i) * 100. / i;
+			cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
+		}
+	}
 }
 
 int main() {
-    Operate();
+	using namespace std::literals;
+	LogDuration total("Total");
+	Operate();
 }
--- sprint6/Lesson4/Lesson4/log_duration.h
+++ sprint6/Lesson4/Lesson4/log_duration.h
@@ -5,22 +5,25 @@
 
 class LogDuration {
 public:
-    // заменим имя типа std::chrono::steady_clock
-    // с помощью using для удобства
-    using Clock = std::chrono::steady_clock;
+	// заменим имя типа std::chrono::steady_clock
+	// с помощью using для удобства
+	using Clock = std::chrono::steady_clock;
 
-    LogDuration() {
-    }
+	LogDuration() = default;
+	LogDuration(std::string msg) : msg_(msg) {
+	}
 
-    ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
+	~LogDuration() {
+		using namespace std::chrono;
+		using namespace std::literals;
 
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-    }
+		const auto end_time = Clock::now();
+		const auto dur = end_time - start_time_;
+		std::cerr << msg_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+	}
 
 private:
-    const Clock::time_point start_time_ = Clock::now();
+	const Clock::time_point start_time_ = Clock::now();
+	std::string msg_;
 };
+
