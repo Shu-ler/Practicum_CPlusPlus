fdb367201de561c8a9ebed0fde68fa69242b7ff2
Filimonov Igor <shuler1970@gmail.com>
2025-08-10 11:01:56 +0700

    Урок готов

---
+++ sprint 6/Topic3/Lesson 2/Lesson 2/Lesson 2.cpp
@@ -0,0 +1,46 @@
+﻿#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+
+// реализуйте эту функцию:
+void CreateFiles(const string& config_file) {
+	ifstream input(config_file);
+	ofstream output;
+	string buf;
+
+	while (getline(input, buf)) {
+
+		if (buf[0] == '>') {
+			output << buf.substr(1) << endl;
+		}
+		else {
+			output.close();
+			output.open(buf);
+		}
+	};
+};
+
+string GetLine(istream& in) {
+	string s;
+	getline(in, s);
+	return s;
+}
+
+int main() {
+	ofstream("test_config.txt"s) << "a.txt\n"
+		">10\n"
+		">abc\n"
+		"b.txt\n"
+		">123"s;
+
+	CreateFiles("test_config.txt"s);
+	ifstream in_a("a.txt"s);
+	assert(GetLine(in_a) == "10"s && GetLine(in_a) == "abc"s);
+
+	ifstream in_b("b.txt"s);
+	assert(GetLine(in_b) == "123"s);
+}
