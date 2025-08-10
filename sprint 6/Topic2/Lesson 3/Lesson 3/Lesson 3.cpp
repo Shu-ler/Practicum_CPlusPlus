b3c5eb8c173eea8886c5fde193c578a326a05620
Filimonov Igor <shuler1970@gmail.com>
2025-08-10 09:53:51 +0700

    Урок 3 готов
    Связь потоков

---
+++ sprint 6/Topic2/Lesson 3/Lesson 3/Lesson 3.cpp
@@ -0,0 +1,29 @@
+﻿#include "log_duration.h"
+
+#include <iostream>
+
+using namespace std;
+
+class StreamUntier {
+public:
+	StreamUntier(std::istream& stream) : stream_(stream) { 	};
+	~StreamUntier() {
+		stream_.tie(tied_before_);
+	};
+
+private:
+	istream& stream_; 
+	ostream* tied_before_ = stream_.tie(nullptr);
+};
+
+int main() {
+	LOG_DURATION("\\n with tie"s);
+
+	StreamUntier guard(cin);
+	int i;
+	while (cin >> i) {
+		cout << i * i << "\n"s;
+	}
+
+	return 0;
+}
