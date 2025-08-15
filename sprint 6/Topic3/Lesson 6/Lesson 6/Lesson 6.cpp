00ef9b02adeaf377903a51d2e2d81ba6da38e16e
Filimonov Igor <shuler1970@gmail.com>
2025-08-15 15:22:47 +0700

    Урок 6 сделан

--- sprint 6/Topic3/Lesson 6/Lesson 6/Lesson 6.cpp
+++ sprint 6/Topic3/Lesson 6/Lesson 6/Lesson 6.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 using filesystem::path;
@@ -12,8 +14,33 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
+void PrintTree(ostream& dst, const path& p, const filesystem::file_status& status, int offset) {
+    dst << string(offset, ' ') << p.filename().string() << endl;
+
+    if (status.type() == filesystem::file_type::directory) {
+        std::vector<filesystem::directory_entry> all_elements;
+
+        for (const auto& dir_entry : filesystem::directory_iterator(p)) {
+            all_elements.push_back(dir_entry);
+        }
+
+        std::sort(all_elements.begin(), all_elements.end(), [](const filesystem::directory_entry& lhs, const filesystem::directory_entry& rhs) {
+            if (lhs.status().type() == rhs.status().type()) {
+                return lhs.path().filename() > rhs.path().filename();
+            }
+            return (lhs.status().type() == filesystem::file_type::directory); });
+
+        for (const auto& dir_entry : all_elements) {
+            PrintTree(dst, dir_entry.path(), dir_entry.status(), offset + 2);
+        }
+    }
+};
+
 // напишите эту функцию
-void PrintTree(ostream& dst, const path& p);
+void PrintTree(ostream& dst, const path& p) {
+    int offset = 0;
+    PrintTree(dst, p, filesystem::status(p), offset);
+};
 
 int main() {
     error_code err;
@@ -27,6 +54,7 @@ int main() {
 
     ostringstream out;
     PrintTree(out, "test_dir"_p);
+    auto aa = out.str();
     assert(out.str() ==
         "test_dir\n"
         "  b\n"
