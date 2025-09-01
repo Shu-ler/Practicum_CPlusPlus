3f4764799adad5358dfc4833785426b5b63b8599
Filimonov Igor <shuler1970@gmail.com>
2025-09-01 22:29:51 +0700

    Готово

--- sprint 7/Topik 3/Lesson 8/Lesson 8/Lesson 8.cpp
+++ sprint 7/Topik 3/Lesson 8/Lesson 8/Lesson 8.cpp
@@ -1,5 +1,6 @@
 ﻿#include <cassert>
 #include <cstdlib>
+#include <utility>
 
 template <typename Type>
 class ArrayPtr {
@@ -10,19 +11,21 @@ public:
     // Создаёт в куче массив из size элементов типа Type.
     // Если size == 0, поле raw_ptr_ должно быть равно nullptr
     explicit ArrayPtr(size_t size) {
-        // TODO: реализуйте конструктор самостоятельно
+        if (size > 0) {
+            raw_ptr_ = new Type[size];
+        }
     }
 
     // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
     explicit ArrayPtr(Type* raw_ptr) noexcept {
-        // TODO: реализуйте конструктор самостоятельно
+        raw_ptr_ = raw_ptr;
     }
 
     // Запрещаем копирование
     ArrayPtr(const ArrayPtr&) = delete;
 
     ~ArrayPtr() {
-        // Напишите деструктор самостоятельно
+        delete[] raw_ptr_;
     }
 
     // Запрещаем присваивание
@@ -31,35 +34,36 @@ public:
     // Прекращает владением массивом в памяти, возвращает значение адреса массива
     // После вызова метода указатель на массив должен обнулиться
     [[nodiscard]] Type* Release() noexcept {
-        // Заглушка. TODO: реализуйте метод самостоятельно
-        return nullptr;
+        Type* ret = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return ret;
     }
 
     // Возвращает ссылку на элемент массива с индексом index
     Type& operator[](size_t index) noexcept {
-        // TODO: реализуйте операцию самостоятельно
+        assert(raw_ptr_);
+        return *(raw_ptr_ + index);
     }
 
     // Возвращает константную ссылку на элемент массива с индексом index
     const Type& operator[](size_t index) const noexcept {
-        // TODO: реализуйте операцию самостоятельно
+        assert(raw_ptr_);
+        return raw_ptr_[index];
     }
 
     // Возвращает true, если указатель ненулевой, и false в противном случае
     explicit operator bool() const {
-        // Заглушка. TODO: реализуйте операцию самостоятельно
-        return false;
+        return raw_ptr_ != nullptr;
     }
 
     // Возвращает значение сырого указателя, хранящего адрес начала массива
     Type* Get() const noexcept {
-        // Заглушка. TODO: реализуйте метод самостоятельно
-        return nullptr;
+        return raw_ptr_;
     }
 
     // Обменивается значениям указателя на массив с объектом other
     void swap(ArrayPtr& other) noexcept {
-        // TODO: реализуйте метод самостоятельно
+        std::swap(raw_ptr_, other.raw_ptr_);
     }
 
 private:
