22678fee8fc8e606237eef77f18c0d5688b7f330
Filimonov Igor <shuler1970@gmail.com>
2025-09-06 01:19:38 +0700

    Решение готово

--- sprint 8/Topik 1/Lesson 7-2/Lesson 7-2/Lesson 7-2.cpp
+++ sprint 8/Topik 1/Lesson 7-2/Lesson 7-2/Lesson 7-2.cpp
@@ -1,6 +1,8 @@
 ﻿#include <cassert>
 #include <iostream>
 #include <string_view>
+#include <map>
+#include <deque>
 
 using namespace std;
 
@@ -11,17 +13,39 @@ public:
     string_view TranslateBackward(string_view target) const;
 
 private:
-
+    map<string_view, string_view> forward_{};
+    map<string_view, string_view> backward_{};
+    deque<string> words_{};
 };
 
 void Translator::Add(string_view source, string_view target) {
+    
+    // Добавляем строки в deque, если их ещё нет там
+    auto& source_str = words_.emplace_back(source);
+    auto& target_str = words_.emplace_back(target);
+
+    // Создаём string_view для ссылок на строки в deque
+    string_view sv_source = source_str;
+    string_view sv_target = target_str;
+
+    // Сохраняем пары переводов в maps
+    forward_[sv_source] = sv_target;
+    backward_[sv_target] = sv_source;
 }
 
 string_view Translator::TranslateForward(string_view source) const {
+    auto it = forward_.find(source);
+    if (it != forward_.end()) {
+        return it->second;
+    }
     return string_view();
 }
 
 string_view Translator::TranslateBackward(string_view target) const {
+    auto it = backward_.find(target);
+    if (it != backward_.end()) {
+        return it->second;
+    }
     return string_view();
 }
 
