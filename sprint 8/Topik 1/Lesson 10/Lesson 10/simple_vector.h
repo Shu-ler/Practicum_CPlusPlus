8bfbfc156389180f87b3c4a1c18e35663acc1c31
Filimonov Igor <shuler1970@gmail.com>
2025-09-07 22:28:44 +0700

    Почти добавлен ArrayPtr

--- sprint 8/Topik 1/Lesson 10/Lesson 10/simple_vector.h
+++ sprint 8/Topik 1/Lesson 10/Lesson 10/simple_vector.h
@@ -2,6 +2,9 @@
 
 #include <cassert>
 #include <initializer_list>
+#include <algorithm>
+#include <stdexcept>
+#include "array_ptr.h"
 
 template <typename Type>
 class SimpleVector {
@@ -16,7 +19,7 @@ public:
 		: items_(size)  // может бросить исключение
 		, size_(size)
 		, capacity_(size) {
-		std::fill(items_.Get(), items_.Get() + size_, Type{});  // Может бросить исключение
+		std::fill(items_.GetRawPtr(), items_.GetRawPtr() + size_, Type{});  // Может бросить исключение
 	}
 
 	// Создаёт вектор из size элементов, инициализированных значением value
@@ -24,7 +27,7 @@ public:
 		: items_(size)  // может бросить исключение
 		, size_(size)
 		, capacity_(size) {
-		std::fill(items_.Get(), items_.Get() + size_, value);  // Может бросить исключение
+		std::fill(items_.GetRawPtr(), items_.GetRawPtr() + size_, value);  // Может бросить исключение
 	}
 
 	// Создаёт вектор из std::initializer_list
@@ -32,57 +35,107 @@ public:
 		: items_(init.size())  // Может бросить исключение
 		, size_(init.size())
 		, capacity_(init.size()) {
-		std::copy(init.begin(), init.end(), items_.Get());
+		std::copy(init.begin(), init.end(), items_.GetRawPtr());
 	}
 
 	// Возвращает количество элементов в массиве
 	size_t GetSize() const noexcept {
-		// TODO: тело самостоятельно
-		return 0;
+		return size_;
 	}
 
 	// Возвращает вместимость массива
 	size_t GetCapacity() const noexcept {
-		// TODO: тело самостоятельно
-		return 0;
+		return capacity_;
 	}
 
 	// Сообщает, пустой ли массив
 	bool IsEmpty() const noexcept {
-		return true;
+		return size_ == 0;
 	}
 
 	// Возвращает ссылку на элемент с индексом index
 	Type& operator[](size_t index) noexcept {
-		// TODO: тело самостоятельно
+		assert(index < size_);
+		return items_[index];
 	}
 
 	// Возвращает константную ссылку на элемент с индексом index
 	const Type& operator[](size_t index) const noexcept {
-		// TODO: тело самостоятельно
+		assert(index < size_);
+		return items_[index];
 	}
 
 	// Возвращает константную ссылку на элемент с индексом index
 	// Выбрасывает исключение std::out_of_range, если index >= size
 	Type& At(size_t index) {
-		// TODO: тело самостоятельно
+		if (index >= size_) {
+			throw std::out_of_range("Index out of range");
+		}
+		return items_[index];
 	}
 
 	// Возвращает константную ссылку на элемент с индексом index
 	// Выбрасывает исключение std::out_of_range, если index >= size
 	const Type& At(size_t index) const {
-		// TODO: тело самостоятельно
+		if (index >= size_) {
+			throw std::out_of_range("Index out of range");
+		}
+		return items_[index];
 	}
 
 	// Обнуляет размер массива, не изменяя его вместимость
 	void Clear() noexcept {
-		// TODO: тело самостоятельно
+		size_ = 0;
 	}
 
 	// Изменяет размер массива.
 	// При увеличении размера новые элементы получают значение по умолчанию для типа Type
+	// TODO Resize
 	void Resize(size_t new_size) {
-		// TODO: тело самостоятельно
+		
+		//if (new_size <= size_) {
+		//	size_ = new_size;
+		//}
+		//else if (new_size <= capacity_) {
+		//	for (size_t i = size_; i < new_size; ++i) {
+		//		*(items_ + i) = Type{};
+		//	}
+		//	size_ = new_size;
+		//}
+		//else {
+		//	Type* temp = new Type[new_size];
+		//	std::fill(temp + size_, temp + new_size, Type{});
+		//	for (size_t i = 0; i < size_; ++i) {
+		//		(temp + i) = (items_ + i);
+		//	}
+		//	/*for (size_t i = size_;i < new_size;++i) {
+		//		*(temp + i) = Type{};
+		//	}*/
+		//	delete[] items_;
+		//	items_ = temp;
+		//	capacity_ = new_size;
+		//	size_ = new_size;
+		//}
+
+		if (new_size > capacity_) {
+			// Вычисляем новую вместимость вектора
+			const size_t new_capacity = std::max(capacity_ * 2, new_size);
+
+			// Копируем существующие элементы вектора на новое место
+			auto new_items =  ReallocateCopy(new_capacity);  // может выбросить исключение
+			// Заполняем добавленные элементы значением по умолчанию
+			std::fill(new_items.Get() + size_, new_items.Get() + new_size, Type{});  // может выбросить исключение
+
+			items_.swap(new_items);
+			capacity_ = new_capacity;
+		}
+		else if (new_size > size_) {
+			assert(new_size <= capacity_);
+			std::fill(items_.GetRawPtr() + size_, items_.GetRawPtr() + new_size, Type{});  // может выбросить исключение
+		}
+
+		// Во всех случаях устанавливается новый размер
+		size_ = new_size;
 	}
 
 	// Возвращает итератор на начало массива
@@ -122,8 +175,7 @@ public:
 	}
 
 private:
-	// Вместо сырого указателя лучше использовать умный указатель, такой как ArrayPtr
-	Type* items_{};
+	ArrayPtr<Type> items_;
 
 	size_t size_ = 0;
 	size_t capacity_ = 0;
