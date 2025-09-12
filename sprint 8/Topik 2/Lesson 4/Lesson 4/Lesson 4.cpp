273a22c544d03c9903aa54301b84b4c73b1f0040
Filimonov Igor <shuler1970@gmail.com>
2025-09-12 12:23:21 +0700

    Готово

--- sprint 8/Topik 2/Lesson 4/Lesson 4/Lesson 4.cpp
+++ sprint 8/Topik 2/Lesson 4/Lesson 4/Lesson 4.cpp
@@ -4,42 +4,79 @@
 #include <numeric>
 #include <vector>
 #include <utility>
-#include <deque>
+#include <list>
 
 using namespace std;
 
 template <typename RandomIt>
 void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
-    vector<typename RandomIt::value_type> pool(first, last);
-    size_t cur_pos = 0;
-    while (!pool.empty()) {
-        *(first++) = std::move(pool[cur_pos]);
-        pool.erase(pool.begin() + cur_pos);
-        if (pool.empty()) {
-            break;
-        }
-        cur_pos = (cur_pos + step_size - 1) % pool.size();
-    }
+	std::list<typename RandomIt::value_type> pool;
+	for (auto it = first; it != last; ++it) {
+		pool.push_back(std::move(*it));
+	}
+
+	auto cur_pos = pool.begin();
+	while (!pool.empty()) {
+		*(first++) = std::move(*cur_pos);
+		if (pool.size() == 1) {
+			break;
+		}
+		const auto next_pos = (cur_pos == std::prev(pool.end())) ? pool.begin() : std::next(cur_pos);
+		pool.erase(cur_pos);
+		cur_pos = next_pos;
+		for (uint32_t step_index = 1; step_index < step_size; ++step_index) {
+			cur_pos = (cur_pos == std::prev(pool.end())) ? pool.begin() : std::next(cur_pos);
+		}
+	}
 }
 
+/* Сохраню авторское решение
+
+template <typename Container, typename ForwardIt>
+ForwardIt LoopIterator(Container& container, ForwardIt pos) {
+    return pos == container.end() ? container.begin() : pos;
+}
+ 
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+	list<typename RandomIt::value_type> pool;
+	for (auto it = first; it != last; ++it) {
+		pool.push_back(move(*it));
+	}
+	auto cur_pos = pool.begin();
+	while (!pool.empty()) {
+		*(first++) = move(*cur_pos);
+		if (pool.size() == 1) {
+			break;
+		}
+		const auto next_pos = LoopIterator(pool, next(cur_pos));
+		pool.erase(cur_pos);
+		cur_pos = next_pos;
+		for (uint32_t step_index = 1; step_index < step_size; ++step_index) {
+			cur_pos = LoopIterator(pool, next(cur_pos));
+		}
+	}
+}
+*/
+
 vector<int> MakeTestVector() {
-    vector<int> numbers(10);
-    iota(begin(numbers), end(numbers), 0);
-    return numbers;
+	vector<int> numbers(10);
+	iota(begin(numbers), end(numbers), 0);
+	return numbers;
 }
 
 void TestIntVector() {
-    const vector<int> numbers = MakeTestVector();
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
-        assert(numbers_copy == numbers);
-    }
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
-        assert(numbers_copy == vector<int>({ 0, 3, 6, 9, 4, 8, 5, 2, 7, 1 }));
-    }
+	const vector<int> numbers = MakeTestVector();
+	{
+		vector<int> numbers_copy = numbers;
+		MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+		assert(numbers_copy == numbers);
+	}
+	{
+		vector<int> numbers_copy = numbers;
+		MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+		assert(numbers_copy == vector<int>({ 0, 3, 6, 9, 4, 8, 5, 2, 7, 1 }));
+	}
 }
 
 // Это специальный тип, который поможет вам убедиться, что ваша реализация
@@ -47,66 +84,66 @@ void TestIntVector() {
 // То, как работает этот класс, мы расскажем далее в нашем курсе
 
 struct CopyTrackingInt {
-    int value;
+	int value;
 
-    CopyTrackingInt(int value) noexcept
-        : value(value) {
-    }
+	CopyTrackingInt(int value) noexcept
+		: value(value) {
+	}
 
-    CopyTrackingInt(const CopyTrackingInt& other) noexcept
-        : value(other.value) {
-        ++kAmountOfCopies;
-    }
+	CopyTrackingInt(const CopyTrackingInt& other) noexcept
+		: value(other.value) {
+		++kAmountOfCopies;
+	}
 
-    CopyTrackingInt& operator=(const CopyTrackingInt& other) noexcept {
-        ++kAmountOfCopies;
+	CopyTrackingInt& operator=(const CopyTrackingInt& other) noexcept {
+		++kAmountOfCopies;
 
-        if (&other == this) {
-            return *this;
-        }
+		if (&other == this) {
+			return *this;
+		}
 
-        value = other.value;
+		value = other.value;
 
-        return *this;
-    }
+		return *this;
+	}
 
-    CopyTrackingInt(CopyTrackingInt&&) = default;
-    CopyTrackingInt& operator=(CopyTrackingInt&&) = default;
+	CopyTrackingInt(CopyTrackingInt&&) = default;
+	CopyTrackingInt& operator=(CopyTrackingInt&&) = default;
 
-    inline static size_t kAmountOfCopies = 0;
+	inline static size_t kAmountOfCopies = 0;
 };
 
 bool operator==(const CopyTrackingInt& lhs, const CopyTrackingInt& rhs) {
-    return lhs.value == rhs.value;
+	return lhs.value == rhs.value;
 }
 
 ostream& operator<<(ostream& os, const CopyTrackingInt& v) {
-    return os << v.value;
+	return os << v.value;
 }
 
 void TestAvoidsCopying() {
-    vector<CopyTrackingInt> numbers;
-    numbers.push_back({ 1 });
-    numbers.push_back({ 2 });
-    numbers.push_back({ 3 });
-    numbers.push_back({ 4 });
-    numbers.push_back({ 5 });
+	vector<CopyTrackingInt> numbers;
+	numbers.push_back({ 1 });
+	numbers.push_back({ 2 });
+	numbers.push_back({ 3 });
+	numbers.push_back({ 4 });
+	numbers.push_back({ 5 });
 
-    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+	MakeJosephusPermutation(begin(numbers), end(numbers), 2);
 
-    vector<CopyTrackingInt> expected;
-    expected.push_back({ 1 });
-    expected.push_back({ 3 });
-    expected.push_back({ 5 });
-    expected.push_back({ 4 });
-    expected.push_back({ 2 });
+	vector<CopyTrackingInt> expected;
+	expected.push_back({ 1 });
+	expected.push_back({ 3 });
+	expected.push_back({ 5 });
+	expected.push_back({ 4 });
+	expected.push_back({ 2 });
 
-    assert(numbers == expected);
-    assert(CopyTrackingInt::kAmountOfCopies == 0);
+	assert(numbers == expected);
+	assert(CopyTrackingInt::kAmountOfCopies == 0);
 }
 
 int main() {
-    TestIntVector();
-    TestAvoidsCopying();
-    return 0;
+	TestIntVector();
+	TestAvoidsCopying();
+	return 0;
 }
