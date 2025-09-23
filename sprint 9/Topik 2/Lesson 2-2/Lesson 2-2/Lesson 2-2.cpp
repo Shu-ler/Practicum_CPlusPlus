582dd26ea2b69a07feae1395bf580fc71ad4a750
Filimonov Igor <shuler1970@gmail.com>
2025-09-23 18:11:13 +0700

    Готово

---
+++ sprint 9/Topik 2/Lesson 2-2/Lesson 2-2/Lesson 2-2.cpp
@@ -0,0 +1,50 @@
+﻿#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+// не меняйте файлы xml.h и xml.cpp
+#include "xml.h"
+
+using namespace std;
+
+struct Spending {
+    string category;
+    int amount;
+};
+
+int CalculateTotalSpendings(const vector<Spending>& spendings) {
+    return accumulate(spendings.begin(), spendings.end(), 0,
+        [](int current, const Spending& spending) {
+            return current + spending.amount;
+        });
+}
+
+string FindMostExpensiveCategory(const vector<Spending>& spendings) {
+    assert(!spendings.empty());
+    auto compare_by_amount = [](const Spending& lhs, const Spending& rhs) {
+        return lhs.amount < rhs.amount;
+        };
+    return max_element(begin(spendings), end(spendings), compare_by_amount)->category;
+}
+
+vector<Spending> LoadFromXml(istream& input) {
+    Document doc = Load(input);
+    vector<Spending> spendings;
+
+    auto nodes = doc.GetRoot().Children();
+    spendings.reserve(nodes.size());
+    for (const Node& node : nodes) {
+        Spending spending{ node.AttributeValue<string>("category"s), node.AttributeValue<int>("amount"s) };
+        spendings.push_back(spending);
+    }
+
+    return spendings;
+}
+
+int main() {
+    const vector<Spending> spendings = LoadFromXml(cin);
+    cout << "Total "sv << CalculateTotalSpendings(spendings) << '\n';
+    cout << "Most expensive is "sv << FindMostExpensiveCategory(spendings) << '\n';
+}
