d2b42b7510fdf69d8993c944c62ea9dcbfb8e738
Filimonov Igor <shuler1970@gmail.com>
2025-09-28 08:16:38 +0700

    Урок 7 - начало

---
+++ sprint 9/Topik 2/Lesson 7/Lesson 7/Lesson 7.cpp
@@ -0,0 +1,15 @@
+﻿#include "libstat.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    TestStatAggregSum();
+    TestStatAggregMax();
+    TestStatAggregMean();
+    TestStatAggregStandardDeviation();
+    TestStatAggregMode();
+    TestStatAggregPrinter();
+
+    cout << "Test passed!"sv << endl;
+}
