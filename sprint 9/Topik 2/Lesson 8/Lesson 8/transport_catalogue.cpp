211b56a6648155358059fe7ed19d34cc4c085cbd
Filimonov Igor <shuler1970@gmail.com>
2025-09-29 17:12:19 +0700

    Классы готовы в первом приближении

--- sprint 9/Topik 2/Lesson 8/Lesson 8/transport_catalogue.cpp
+++ sprint 9/Topik 2/Lesson 8/Lesson 8/transport_catalogue.cpp
@@ -1,1 +1,92 @@
 #include "transport_catalogue.h"
+#include <unordered_set>
+#include <optional>
+#include <utility>
+
+namespace bus {
+
+	Bus::Bus(const std::string& name, const RouteStops& stops) :
+		name_(name),
+		stops_(stops) {
+	}
+
+	const RouteStops& Bus::GetStops() const {
+		return stops_;
+	}
+
+	void Bus::SetStops(const RouteStops& stops) {
+		stops_ = stops;
+	}
+
+	void Bus::AddStop(StopPtr stop) {
+		stops_.push_back(stop);
+	}
+
+	RouteStat Bus::GetStat() const {
+		RouteStat stat{};
+
+		// Используем множество для хранения уникальных остановок
+		std::unordered_set<std::string_view> seen_stops{};
+		std::optional<Coordinates> prev_pos{};
+
+		for (auto stop : stops_) {
+
+			// Увеличиваем общее количество остановок
+			++stat.total_stops;
+
+			// Проверяем, является ли остановка уникальной
+			if (seen_stops.count(stop->name) == 0) {
+				++stat.unique_stops; // Увеличиваем количество уникальных остановок
+				seen_stops.insert(stop->name);
+			}
+
+			// Если есть предыдущая позиция, вычисляем расстояние между остановками
+			if (prev_pos) {
+				stat.route_length += ComputeDistance(*prev_pos, stop->position);
+			}
+
+			// Обновляем предыдущую позицию
+			prev_pos = stop->position;
+		}
+
+		return stat;
+	}
+
+} // namespace bus
+
+namespace trans_catalogue {
+
+	void TransportCatalogue::AddStop(std::string name, Coordinates pos) {
+		// Добавляем остановку в вектор остановок
+		stops_.push_back({ std::move(name), pos });
+
+		// Получаем указатель на добавленную остановку
+		StopPtr added_ptr = &stops_.back();
+
+		// Обновляем ассоциативный массив для быстрого доступа по имени остановки
+		stop_by_name_[added_ptr->name] = added_ptr;
+	}
+
+	void TransportCatalogue::AddBus(std::string name, std::vector<StopPtr> stops) {
+		// Добавляем маршрут в вектор маршрутов
+		buses_.push_back({ std::move(name), std::move(stops) });
+
+		// Получаем указатель на добавленный маршрут
+		BusPtr added_ptr = &buses_.back();
+
+		// Обновляем ассоциативный массив для быстрого доступа по имени маршрута
+		bus_by_name_[added_ptr->name] = added_ptr;
+	}
+
+	StopPtr TransportCatalogue::FindStop(std::string_view stop_name) const {
+		auto iter = stop_by_name_.find(stop_name);
+		return (iter == stop_by_name_.end()) ? nullptr : iter->second;
+	}
+
+	BusPtr TransportCatalogue::FindBus(std::string_view bus_name) const {
+		auto iter = bus_by_name_.find(bus_name);
+		return (iter == bus_by_name_.end()) ? nullptr : iter->second;
+	}
+
+} // namespace trans_catalogue
+
--- sprint 9/Topik 2/Lesson 8/Lesson 8/transport_catalogue.h
+++ sprint 9/Topik 2/Lesson 8/Lesson 8/transport_catalogue.h
@@ -3,10 +3,19 @@
 #include "geo.h"
 #include <string>
 #include <iostream>
+#include <vector>
+#include <deque>
+#include <unordered_map>
+
+// Используя 'using', определяем псевдонимы для удобства
+using StopPtr = const stop::Stop*;
+using BusPtr = const bus::Bus*;
+using RouteStat = trans_catalogue::TransportCatalogue::RouteStatistics;
+using RouteStops = std::vector<StopPtr>;
 
 namespace stop {
 
-	/*
+	/**
 	 * Класс представляет собой модель остановки общественного транспорта.
 	 * Содержит информацию о названии остановки и её географических координатах.
 	 */
@@ -24,6 +33,15 @@ namespace stop {
 			name_(name),
 			coordinates_{ lat, lon } {
 		};
+
+		// Инициализирует новую остановку с заданными именем и координатами.
+		// @param name название остановки
+		// @param pos географические координаты
+		Stop(const std::string& name, const Coordinates pos) :
+			name_(name),
+			coordinates_{ pos } {
+		};
+
 
 		// Возвращает название остановки.
 		// @return название остановки
@@ -44,7 +62,7 @@ namespace stop {
 			coordinates_ = { lat, lon };
 		}
 
-		// Метод проверки равенства остановок
+		// Переопределение оператора равенства для остановок остановок
 		// @param other вторая остановка
 		bool operator==(const Stop& other) const {
 			return name_ == other.name_ && coordinates_ == other.coordinates_;
@@ -55,9 +73,9 @@ namespace stop {
 		// @param stop остановка
 		friend std::ostream& operator<<(std::ostream& os, const Stop& stop) {
 			using namespace std::literals;
-			os << "Stop: "s << stop.name_ 
-				<< ", Coordinates: ("s << stop.coordinates_.lat 
-				<< ", "s << stop.coordinates_.lng 
+			os << "Stop: "s << stop.name_
+				<< ", Coordinates: ("s << stop.coordinates_.lat
+				<< ", "s << stop.coordinates_.lng
 				<< ")"s;
 			return os;
 		}
@@ -69,14 +87,75 @@ namespace stop {
 
 } // namespace stop
 
-class TransportCatalogue {
+namespace bus {
 
-	// TODO: Метод добавления маршрута в базу
-	// TODO: Метод добавления остановки в базу
-	// TODO: Метод поиска маршрута по имени
-	// TODO: Метод поиска остановки по имени
-	// TODO: Метод получения информации о маршруте
+	/**
+	 * Класс Bus моделирует автобусный маршрут и содержит следующие компоненты : 
+	 *  - name_ : строка, хранящая название маршрута.
+	 *  - stops_ : вектор указателей на остановки(StopPtr), 
+	 *		представляющий последовательность остановок в маршруте.
+	 */
+	class Bus {
+	public:
+		// Конструктор по умолчанию
+		Bus() = default;
 
-private:
-	// TODO: Члены класса
-};
+		// Параметрический конструктор
+		Bus(const std::string& name, const RouteStops& stops);
+
+		// Getter для stops_
+		const RouteStops& GetStops() const;
+
+		// Setter для stops_
+		void SetStops(const RouteStops& stops);
+
+		// Метод для добавления остановки
+		void AddStop(StopPtr stop);
+
+		// Метод для получения статистики по маршруту (количество остановок, 
+		// уникальные остановки, длина маршрута).
+		RouteStat GetStat() const;
+
+	private:
+		std::string name_;	// название маршрута
+		RouteStops stops_;	// вектор указателей на остановки
+	};
+
+} // namespace bus
+
+namespace trans_catalogue {
+
+	class TransportCatalogue {
+	public:
+
+		struct RouteStatistics {
+			size_t total_stops = 0;      // Общее количество остановок в маршруте
+			size_t unique_stops = 0;     // Количество уникальных остановок
+			double route_length = 0.0;   // Длина маршрута в метрах
+		};
+
+		// Метод добавления остановки в справочник
+		void AddStop(std::string name, Coordinates pos);
+
+		// Метод добавления маршрута в справочник
+		void AddBus(std::string name, std::vector<StopPtr> stops);
+
+		// Метод поиска остановки по имени
+		StopPtr FindStop(std::string_view bus_name) const;
+
+		// Метод поиска маршрута по имени
+		BusPtr FindBus(std::string_view bus_name) const;
+
+	private:
+		// Контейнер и 'индекс' остановок
+		std::deque<stop::Stop> stops_{};
+		std::unordered_map<std::string_view, StopPtr> stop_by_name_{};
+
+		// Контейнер и 'индекс' маршрутов
+		std::deque<bus::Bus> buses_{};
+		std::unordered_map<std::string_view, BusPtr> bus_by_name_{};
+	};
+
+} // namespace trans_catalogue
+
+
