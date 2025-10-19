91e8a7713250624b3a8c21c4491d20f1c5c9b9de
Filimonov Igor <shuler1970@gmail.com>
2025-10-19 12:24:33 +0700

    Выполнение оптимизаций
    Готово

---
+++ sprint 10/Topik 2/Lesson 3/Lesson 3/Lesson 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdint>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int64_t a;
+    int64_t b;
+
+    // Считывание двух слагаемых
+    cin >> a >> b;
+
+    // Проверка переполнения и вывод
+    if ((a > 0 && b > 0 && a > numeric_limits<std::int64_t>::max() - b) 
+        || (a < 0 && b < 0 && a < numeric_limits<std::int64_t>::min() - b)) {
+        cout << "Overflow!"s << endl;
+    }
+    else {
+        cout << a + b << endl;
+    }
+}
