237455711271d849382ff0e2c93b7159a24d2957
Filimonov Igor <shuler1970@gmail.com>
2025-10-31 12:36:47 +0700

    Шутка

---
+++ sprint 11/Topic 1/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+class HelloMsg {
+public:
+	explicit HelloMsg(std::ostream& out) noexcept {
+		using namespace std::literals;
+		out << "Hello World!"s
+			<< std::endl;
+	};
+	~HelloMsg() {};
+};
+
+int main() {
+    HelloMsg hello(std::cout);
+}
