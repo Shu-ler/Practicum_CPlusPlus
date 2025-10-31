6eeaf6a283f630b043cbbdf278ef9fdc0e67a1c8
Filimonov Igor <shuler1970@gmail.com>
2025-10-31 12:42:53 +0700

    Step 1
    Lesson 6

---
+++ sprint 11/Topic 1/SVG/SVG/svg.cpp
@@ -0,0 +1,172 @@
+#include "svg.h"
+
+// Экранирует специальные символы SVG
+std::string EscapeSvgText(std::string_view text) {
+    std::string result;
+    for (char c : text) {
+        switch (c) {
+        case '"':  result += "&quot;";  break;
+        case '<':  result += "&lt;";    break;
+        case '>':  result += "&gt;";    break;
+        case '&':  result += "&amp;";   break;
+        case '\'':  result += "&apos;"; break;
+
+        default:   result += c; break;
+        }
+    }
+    return result;
+}
+
+namespace svg {
+
+    using namespace std::literals;
+
+    void Object::Render(const RenderContext& context) const {
+        context.RenderIndent();
+
+        // Делегируем вывод тега своим подклассам
+        RenderObject(context);
+
+        context.out << std::endl;
+    }
+
+    // ---------- Circle ------------------
+
+    Circle& Circle::SetCenter(Point center) {
+        center_ = center;
+        return *this;
+    }
+
+    Circle& Circle::SetRadius(double radius) {
+        radius_ = radius;
+        return *this;
+    }
+
+    void Circle::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+
+        context.RenderIndent();
+
+        out << "<circle cx=\""sv
+            << center_.x
+            << "\" cy=\""sv
+            << center_.y
+            << "\" "sv
+            << "r=\""sv
+            << radius_
+            << "\" "sv
+            << "/>"sv;
+    }
+
+    // ---------- Polyline ------------------
+
+    Polyline& Polyline::AddPoint(Point point) {
+        points_.push_back(point);
+        return *this;
+    }
+
+    void Polyline::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+
+        context.RenderIndent();
+
+        out << "<polyline points=\""sv;
+        bool first = true;
+        for (const Point& point : points_) {
+            if (first) {
+                first = false;
+            }
+            else {
+                out << ' ';
+            }
+            out << point.x
+                << ','
+                << point.y;
+        }
+        out << "\" "sv
+            << "/>"sv;
+    }
+
+    // ---------- Polyline ------------------
+
+    Text& Text::SetPosition(Point pos) {
+        position_ = pos;
+        return *this;
+    }
+
+    Text& Text::SetOffset(Point offset) {
+        offset_ = offset;
+        return *this;
+    }
+
+    Text& Text::SetFontSize(uint32_t size) {
+        size_ = size;
+        return *this;
+    }
+
+    Text& Text::SetFontFamily(std::string font_family) {
+        font_family_ = std::move(font_family);
+        return *this;
+    }
+
+    Text& Text::SetFontWeight(std::string font_weight) {
+        font_weight_ = std::move(font_weight);
+        return *this;
+    }
+
+    Text& Text::SetData(std::string data) {
+        data_ = std::move(data);
+        return *this;
+    }
+
+    void Text::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+
+        context.RenderIndent();
+
+        // Вывод обязательных атрибутов
+        out << "<text "
+            << "x=\"" << position_.x << "\" "
+            << "y=\"" << position_.y << "\" "
+            << "dx=\"" << offset_.x << "\" "
+            << "dy=\"" << offset_.y << "\" "
+            << "font-size=\"" << size_ << "\"";
+
+        // Вывод атрибутов с контролем заполнения
+        if (!font_family_.empty()) {
+            out << " font-family=\"" << font_family_ << "\"";
+        }
+
+        if (!font_weight_.empty()) {
+            out << " font-weight=\"" << font_weight_ << "\"";
+        }
+
+        // Вывод собственно текста
+        out << ">"
+            << EscapeSvgText(data_)
+            << "</text>"
+            << std::endl;
+    }
+
+    // ---------- Document ------------------
+
+    void Document::AddPtr(std::unique_ptr<Object>&& obj) {
+        objects_.emplace_back(std::move(obj));
+    }
+
+    void Document::Render(std::ostream& out) const {
+
+        // Вывод заголовка файла
+        out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+        out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n";
+
+        // Вывод содержимого файла
+        for (const auto& obj : objects_) {
+            obj->Render(RenderContext(out, 2));
+        }
+
+        // Вывод концовки файла
+        out << "</svg>\n";
+    }
+
+}  // namespace svg
---
+++ sprint 11/Topic 1/SVG/SVG/svg.h
@@ -0,0 +1,171 @@
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace svg {
+
+    /*
+     * Вспомогательная структура, представляющая координаты точки на плоскости.
+     * Используется для определения позиций элементов в SVG-графике
+     */
+
+    struct Point {
+        Point() = default;
+        Point(double x, double y)
+            : x(x)
+            , y(y) {
+        }
+
+        double x = 0;
+        double y = 0;
+    };
+
+    /*
+     * Вспомогательная структура, хранящая контекст для вывода SVG-документа с отступами.
+     * Хранит ссылку на поток вывода, текущее значение и шаг отступа при выводе элемента
+     */
+    struct RenderContext {
+        RenderContext(std::ostream& out)
+            : out(out) {
+        }
+
+        RenderContext(std::ostream& out, int indent_step, int indent = 0)
+            : out(out)
+            , indent_step(indent_step)
+            , indent(indent) {
+        }
+
+        RenderContext Indented() const {
+            return { out, indent_step, indent + indent_step };
+        }
+
+        void RenderIndent() const {
+            for (int i = 0; i < indent; ++i) {
+                out.put(' ');
+            }
+        }
+
+        std::ostream& out;
+        int indent_step = 0;
+        int indent = 0;
+    };
+
+    /*
+     * Абстрактный базовый класс Object служит для унифицированного хранения
+     * конкретных тегов SVG-документа
+     * Реализует паттерн "Шаблонный метод" для вывода содержимого тега
+     */
+    class Object {
+    public:
+        void Render(const RenderContext& context) const;
+
+        virtual ~Object() = default;
+
+    private:
+        // Чисто виртуальный метод для предоставления унифицированного интерфейса
+        // для вывода SVG-тегов, реализуемого в производных классах
+        virtual void RenderObject(const RenderContext& context) const = 0;
+    };
+
+    /*
+     * Класс Circle моделирует элемент <circle> для отображения круга
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
+     */
+    class Circle final : public Object {
+    public:
+        // Установка центра круга (cx, cy)
+        Circle& SetCenter(Point center);
+
+        // Установка радиуса круга (r)
+        Circle& SetRadius(double radius);
+
+    private:
+        // Вывод координат центра круга (cx, cy) и радиуса (r) в формате SVG
+        void RenderObject(const RenderContext& context) const override;
+
+    private:
+        Point center_;
+        double radius_ = 1.0;
+    };
+
+    /*
+     * Класс Polyline моделирует элемент <polyline> для отображения ломаных линий
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/polyline
+     */
+    class Polyline final : public Object {
+    public:
+        // Добавляет очередную вершину к ломаной линии
+        Polyline& AddPoint(Point point);
+
+    private:
+        // Вывод всех точек ломаной линии в формате SVG
+        void RenderObject(const RenderContext& context) const override;
+
+    private:
+        std::vector<Point> points_;
+    };
+
+    /*
+     * Класс Text моделирует элемент <text> для отображения текста
+     * https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
+     */
+    class Text final : public Object {
+    public:
+        // Задаёт координаты опорной точки (атрибуты x и y)
+        Text& SetPosition(Point pos);
+
+        // Задаёт смещение относительно опорной точки (атрибуты dx, dy)
+        Text& SetOffset(Point offset);
+
+        // Задаёт размеры шрифта (атрибут font-size)
+        Text& SetFontSize(uint32_t size);
+
+        // Задаёт название шрифта (атрибут font-family)
+        Text& SetFontFamily(std::string font_family);
+
+        // Задаёт толщину шрифта (атрибут font-weight)
+        Text& SetFontWeight(std::string font_weight);
+
+        // Задаёт текстовое содержимое объекта (отображается внутри тега <text>)
+        Text& SetData(std::string data);
+
+    private:
+        void RenderObject(const RenderContext& context) const override;
+
+    private:
+        Point position_{ 0,0 };     // Координаты опорной точки (атрибуты x и y)
+        Point offset_{ 0,0 };       // Cмещение относительно опорной точки (атрибуты dx, dy)
+        uint32_t size_ = 1;         // Размер шрифта (атрибут font-size)
+        std::string font_family_{}; // Название шрифта (атрибут font-family)
+        std::string font_weight_{}; // Толщина шрифта (атрибут font-weight)
+        std::string data_{};        // Содержимое объекта (отображается внутри тега <text>)
+    };
+
+    class Document {
+    public:
+        /*
+         Метод Add добавляет в svg-документ любой объект-наследник svg::Object.
+         Пример использования:
+         Document doc;
+         doc.Add(Circle().SetCenter({20, 30}).SetRadius(15));
+        */
+        template <typename ObjectType>
+        void Add(ObjectType object) {
+            AddPtr(std::make_unique<ObjectType>(std::move(object)));
+        }
+
+        // Добавляет в svg-документ объект-наследник svg::Object
+        void AddPtr(std::unique_ptr<Object>&& obj);
+
+        // Выводит в ostream svg-представление документа
+        void Render(std::ostream& out) const;
+
+    private:
+        std::vector<std::unique_ptr<Object>> objects_;
+    };
+
+}  // namespace svg
