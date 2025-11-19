9d445b9ee13144d0adbdf10d0b872bcd886a5283
Filimonov Igor <shuler1970@gmail.com>
2025-11-19 21:14:30 +0700

    RAII - готово

--- sprint 12/Topic 2/Lesson 5-2/Lesson 5-2/booking.h
+++ sprint 12/Topic 2/Lesson 5-2/Lesson 5-2/booking.h
@@ -1,21 +1,62 @@
 #pragma once
 
-template <typename Provider>
-class Booking {
-private:
-    using BookingId = typename Provider::BookingId;
+namespace raii {
 
-    Provider* provider_;
-    ? ? ?
+    template <typename Provider>
+    class Booking {
+    private:
+        using BookingId = typename Provider::BookingId;
 
-public:
-    Booking(Provider * p, ? ? ? );
-    Booking(const Booking&);
-    Booking(Booking&& other);
-    Booking& operator=(const Booking&);
-    Booking& operator=(Booking&& other);
-    ~Booking();
+        Provider* provider_ = nullptr;
+        BookingId booking_id_;
 
-    // Эта функция не требуется в тестах, но в реальной программе она может быть нужна
-    BookingId GetId() const;
-};
+    public:
+        // Конструктор
+        Booking(Provider* p, BookingId id)
+            : provider_(p)
+            , booking_id_(id) {
+        }
+
+        // Запрещаем копирование
+        Booking(const Booking&) = delete;
+        Booking& operator=(const Booking&) = delete;
+
+        // Перемещение
+        Booking(Booking&& other) noexcept
+            : provider_(other.provider_)
+            , booking_id_(other.booking_id_) {
+            // Обнуляем исходный объект, чтобы деструктор не вызвал CancelOrComplete дважды
+            other.provider_ = nullptr;
+        }
+
+        Booking& operator=(Booking&& other) noexcept {
+            if (this != &other) {
+                // Отменяем свою бронь (если была)
+                if (provider_) {
+                    provider_->CancelOrComplete(*this);
+                }
+
+                // Забираем чужую
+                provider_ = other.provider_;
+                booking_id_ = other.booking_id_;
+
+                // Обнуляем чужой
+                other.provider_ = nullptr;
+            }
+            return *this;
+        }
+
+        // Деструктор
+        ~Booking() {
+            if (provider_) {
+                provider_->CancelOrComplete(*this);
+            }
+        }
+
+        // Получение ID
+        BookingId GetId() const {
+            return booking_id_;
+        }
+    };
+
+} // namespace raii
