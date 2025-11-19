0c9ecb38eea529ec2e2336bc6dae05706bb2913a
Filimonov Igor <shuler1970@gmail.com>
2025-11-19 09:45:13 +0700

    Готово

--- sprint 12/Topic 2/Lesson 3/Lesson 3/Lesson 3.cpp
+++ sprint 12/Topic 2/Lesson 3/Lesson 3/Lesson 3.cpp
@@ -6,24 +6,89 @@ using namespace std;
 // Реализуйте шаблон класса UniquePtr
 template <typename T>
 class UniquePtr {
-private:
-    // ???
 public:
-    UniquePtr();
-    explicit UniquePtr(T* ptr);
-    UniquePtr(const UniquePtr&);
-    UniquePtr(UniquePtr&& other);
-    UniquePtr& operator=(const UniquePtr&);
-    UniquePtr& operator=(nullptr_t);
-    UniquePtr& operator=(UniquePtr&& other);
-    ~UniquePtr();
+    // Конструктор по умолчанию — пустой указатель
+    UniquePtr()
+        : ptr_(nullptr) {
+    }
 
-    T& operator*() const;
-    T* operator->() const;
-    T* Release();
-    void Reset(T* ptr);
-    void Swap(UniquePtr& other);
-    T* Get() const;
+    // Конструктор, захватывающий указатель
+    explicit UniquePtr(T* ptr)
+        : ptr_(ptr) {
+    }
+
+    // Конструктор копирования - запрещаем
+    UniquePtr(const UniquePtr&) = delete;
+
+    // Конструктор перемещения
+    UniquePtr(UniquePtr&& other) : ptr_(other.ptr_) {
+        other.ptr_ = nullptr;
+    }
+
+    // Оператор копирования - запрещаем
+    UniquePtr& operator=(const UniquePtr&) = delete;
+
+    // Оператор присваивания с nullptr
+    UniquePtr& operator=(nullptr_t) {
+        Reset(nullptr);
+        return *this;
+    }
+
+    // Move-оператор присваивания
+    UniquePtr& operator=(UniquePtr&& other) {
+        if (this != &other) {
+            Reset(other.ptr_);
+            other.ptr_ = nullptr;
+        }
+        return *this;
+    }
+    
+    // Деструктор - удаление объекта владения
+    ~UniquePtr() {
+        delete ptr_;
+    }
+
+    // Оператор разыменования
+    T& operator*() const {
+        assert(ptr_ != nullptr);
+        return *ptr_;
+    }
+    
+    // Оператор стрелки
+    T* operator->() const {
+        assert(ptr_ != nullptr);
+        return ptr_;
+    }
+
+    // Release: отдаёт указатель, перестаёт им владеть
+    T* Release() {
+        T* raw_ptr = ptr_;
+        ptr_ = nullptr;
+        return raw_ptr;
+    }
+
+    // Reset: освобождает старый указатель, захватывает новый
+    void Reset(T* ptr) {
+        if (ptr_ != ptr) {
+            delete ptr_;
+            ptr_ = ptr;
+        }
+    }
+
+    // Swap: обмен с другим UniquePtr
+    void Swap(UniquePtr& other) {
+        T* temp = ptr_;
+        ptr_ = other.ptr_;
+        other.ptr_ = temp;
+    }
+    
+    // Get: возвращает указатель (не отбирая владение)
+    T* Get() const {
+        return ptr_;
+    }
+
+private:
+    T* ptr_;
 };
 
 struct Item {
