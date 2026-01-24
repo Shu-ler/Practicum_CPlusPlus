df395f5de7b677bbe74081b48e9a3a0477d31e19
Filimonov Igor <i.filimonov@digsol.tech>
2026-01-24 15:45:17 +0700

    Лессон 10 готов

---
+++ sprint 14/Topik 2/Variadic Templates/Variadic Templates/equals_to_one_of.cpp
@@ -0,0 +1,17 @@
+﻿#include <cassert>
+#include <string>
+#include <string_view>
+
+using namespace std::literals;
+
+// Шаблонная функция, возвращающая true, если первый аргумент равен хотя бы одному из остальных
+template <typename T, typename... Args>
+bool EqualsToOneOf(const T& value, const Args&... args) {
+    return ((value == args) || ...);  // Свёртка с оператором ||
+}
+
+int main() {
+    assert(EqualsToOneOf("hello"sv, "hi"s, "hello"s));
+    assert(!EqualsToOneOf(1, 10, 2, 3, 6));
+    assert(!EqualsToOneOf(8));
+}
